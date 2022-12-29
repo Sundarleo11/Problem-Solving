5bf821e56aed3cd285aedfa01e0bfc4685e39436
sundarleo11 <sundarrock1999@gmail.com>
2022-12-29 17:35:25 +0530

    HelloWorld

---
+++ Problem_Solving/src/Fundaments/HelloWorld.java
@@ -0,0 +1,11 @@
+package Fundaments;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   System.out.println("Hello World");
+
+	}
+
+}
