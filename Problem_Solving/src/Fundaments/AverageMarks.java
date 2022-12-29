1e95f95e9ac8042dad86ddf329ba95ebe4b05913
sundarleo11 <sundarrock1999@gmail.com>
2022-12-29 17:55:20 +0530

    AverageMarks

---
+++ Problem_Solving/src/Fundaments/AverageMarks.java
@@ -0,0 +1,23 @@
+package Fundaments;
+
+import java.util.Scanner;
+
+public class AverageMarks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner sc =new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		int m1=sc.nextInt();
+		int m2=sc.nextInt();
+		int m3=sc.nextInt();
+		
+		int Avg=(m1+m2+m3)/3;
+		System.out.println(ch);
+		System.out.println(Avg);
+		
+	}
+
+}
