2d951df4e5ee87b2c8f33e403022048c252c2b29
sundarleo11 <sundarrock1999@gmail.com>
2023-01-02 12:43:30 +0530

    KnowledgeTestforLoopingCondition

---
+++ Problem_Solving/src/Fundaments/loopTest.java
@@ -0,0 +1,41 @@
+package Fundaments;
+
+public class loopTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		///Whichof the following codes gives same output:
+		
+		 /*  int i=1;
+		   while(i<5)
+		   {
+		       System.out.println(2*i);
+		       i=i+1;
+		   }*/
+	
+	/*	   int i=2;
+		   while(i<10)
+		   {
+		       System.out.print(i);
+		       i=i+2;
+		   }*/
+		
+		/*   int i=2;
+		   while(i<10)
+		   {
+		       System.out.print(i);
+		       i*=2;
+		   }*/
+		
+		   int i=10;
+		   while(i>0)
+		   {
+		       if(i%2==0)
+		       {
+		           System.out.print(10-i);
+		       }
+		       i--;
+		   }
+	}
+
+}
