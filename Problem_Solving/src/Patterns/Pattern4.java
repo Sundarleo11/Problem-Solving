5a355d981ea35ac7d5cf6469bab1ddad3d39062b
sundarleo11 <sundarrock1999@gmail.com>
2023-01-03 09:52:08 +0530

    Pattern4

---
+++ Problem_Solving/src/Patterns/Pattern4.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		int i = 1;
+		while(i<=n) {
+			int j=1;
+			while(j<=n) {
+				System.out.print(n-j+1);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
