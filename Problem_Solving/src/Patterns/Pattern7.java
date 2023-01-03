78b795cb5726819cc22a43aa01444e4f52f6beb0
sundarleo11 <sundarrock1999@gmail.com>
2023-01-03 12:27:35 +0530

    pattern7

---
+++ Problem_Solving/src/Patterns/Pattern7.java
@@ -0,0 +1,28 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		int i = 1;
+		int p=1;
+		while(i<=n) {
+			int j=1;
+			while(j<=i) {
+				System.out.print(p);
+				p++;
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
