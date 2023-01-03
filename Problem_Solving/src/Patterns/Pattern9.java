66548f36f79fc5b0044a977631898f7df4b9df85
sundarleo11 <sundarrock1999@gmail.com>
2023-01-03 18:32:30 +0530

    Alpha Pattern

---
+++ Problem_Solving/src/Patterns/Pattern9.java
@@ -0,0 +1,31 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		int i = 1;
+		//int p=1;
+		while(i<=n) {
+	
+			int j=1;
+			while(j<=i) {
+				//System.out.print("*");
+				char ch= (char) ((char)'A'+i-1);
+				System.out.print(ch);
+			
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
