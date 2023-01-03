ea69e544d90cc954a8b4f699a3ed47d6b93b121d
sundarleo11 <sundarrock1999@gmail.com>
2023-01-03 09:33:00 +0530

    Pattern3

---
+++ Problem_Solving/src/Patterns/Pattern3.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		
+		int i = 1;
+		while(i<=n) {
+			int j=1;
+			while(j<=n) {
+				System.out.print(j);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
