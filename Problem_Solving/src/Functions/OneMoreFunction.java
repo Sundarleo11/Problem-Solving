e9b4470e5f21b9e1fb026f2fe439d4e8be22b530
sundarleo11 <sundarrock1999@gmail.com>
2023-01-08 22:47:27 +0530

    functionTest

---
+++ Problem_Solving/src/Functions/OneMoreFunction.java
@@ -0,0 +1,27 @@
+package Functions;
+
+public class OneMoreFunction {
+	
+	
+	public static void sum(int a,int b) {
+		int sum =a+b;
+		System.out.println(sum);
+	}
+
+	public static void multi() {
+		int multi=4*4;
+		System.out.println(multi);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=20;
+		 sum(a,b);
+		 
+		 multi();
+		
+		
+
+	}
+
+}
